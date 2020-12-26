a8bc7f66b7f08c700c5b61511a3a703520e52be2
kamiya-s-max <s.kamiya.f15@gmail.com>
2020-12-26 14:32:56 +0900

    アロー関数

--- src/function.ts
+++ src/function.ts
@@ -1,8 +1,6 @@
 export {};
 
-let bmi: (height: number, weight: number) => number
-= function(height: number, weight: number): number {
-  return weight / (height * height)
-}
+let bmi = (height: number, weight: number): number => weight / (height * height);
+
 
 console.log(bmi(1.78, 86))
