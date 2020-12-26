4c1977c7337b4cbeacbd903623485ef773979394
kamiya-s-max <s.kamiya.f15@gmail.com>
2020-12-26 17:46:55 +0900

    アクセス修飾子

--- src/class1.ts
+++ src/class1.ts
@@ -1,18 +1,33 @@
 export {};
 
 class Person {
-  name: string;
-  age: number;
+  public name: string;
+  private age: number;
+  protected nationality: string;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, nationality: string) {
     this.name = name;
     this.age = age;
+    this.nationality = nationality
   }
 
   profile(): string {
-    return `name: ${this.name} age${this.age}`;
+    return `name: ${this.name} age: ${this.age}`;
   }
 }
 
-let taro = new Person('tato', 10);
+class Andloid extends Person {
+
+  constructor(name: string, age: number, nationality: string) {
+    super(name, age, nationality);
+  }
+
+  profile(): string {
+    return `name: ${this.name} age: ${this.age} natinal: ${this.nationality}`;
+  }
+}
+
+let taro = new Person('taro', 10, 'japan');
+console.log(taro.name);
 console.log(taro.profile());
+// console.log(taro.age)
