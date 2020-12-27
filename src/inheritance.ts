4bbb050a9b4b748a098749ec799b9658f19c7576
kamiya-s-max <s.kamiya.f15@gmail.com>
2020-12-27 14:02:04 +0900

    inheritance

---
+++ src/inheritance.ts
@@ -0,0 +1,26 @@
+export {};
+
+class Animal {
+  constructor(public name: string) {}
+  run(): string {
+    return 'run';
+  }
+}
+
+class Lion extends Animal {
+  public speed: number;
+
+  constructor(name: string, speed: number) {
+    super(name);
+    this.speed = speed;
+  }
+  run(): string {
+    return `${super.run()} ${this.speed}km`;
+  }
+}
+
+let animal = new Animal('mic');
+console.log(animal.run())
+
+let lion = new Lion('sinba', 80);
+console.log(lion.run());
